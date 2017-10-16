a30d941018f732a72e067415269dd796670e3e64
Danielle Palmer <daniellezpalmer@gmail.com>
2017-10-16 09:36:18 -0500

    Styled cloth diaper component

--- src/component/ClothDiapers.js
+++ src/component/ClothDiapers.js
@@ -7,37 +7,75 @@ import img33 from '../images/tophatcdd.jpg';
 export default class Cd extends Component {
 
   render() {
+
+    let title = {
+      textAlign:"center",
+      fontSize:60
+    }
+
+    let cd = {
+      display:"flex",
+      textAlign:"center",
+      justifyContent:"space-around"
+    }
+
+    let divs = {
+      textAlign:"center",
+      fontSize:40
+    }
+
+    let img1 = {
+    backgroundImage:`url(${img11})`,
+    height:300,
+    width:300,
+    backgroundRepeat:"no-repeat"
+    }
+
+    let img2 = {
+    backgroundImage:`url(${img22})`,
+    height:300,
+    width:300,
+    backgroundRepeat:"no-repeat"
+    }
+
+    let img3 = {
+    backgroundImage:`url(${img33})`,
+    height:300,
+    width:300,
+    backgroundRepeat:"no-repeat"
+    }
+
     return (
       <div className="Cd">
-        <div>
+        <div style={title}>
             All Things Cloth!<br></br>
             <Link to="/home"><button>Home</button></Link>
-<div>
-  <div>
-  <div>
-  </div>
-  Cloth Diaper<br></br>
-  Color: Osa<br></br>
-  Price: $20.95<br></br>
-  <button>Purchase</button>
-</div>
-<div>
-  <div>
-  </div>
-  Cloth Diaper<br></br>
-  Color: Chico<br></br>
-  Price: $20.95<br></br>
-  <button>Purchase</button>
-</div>
-<div>
-  <div>
-  </div>
-  Cloth Diaper<br></br>
-  Color: TopHat<br></br>
-  Price: $20.95<br></br>
-  <button>Purchase</button>
-</div>
-</div>
+    <div style={cd}>
+    <div style={divs}>
+    <div style={img1}>
+    </div>
+    Cloth Diaper<br></br>
+    Color: Osa<br></br>
+    Price: $20.95<br></br>
+    <button>Purchase</button>
+    </div>
+    <div style={divs}>
+    <div style={img2}>
+    </div>
+    Cloth Diaper<br></br>
+    Color: Chico<br></br>
+    Price: $20.95<br></br>
+    <button>Purchase</button>
+    </div>
+    <div style={divs}>
+    <div style={img3}>
+    </div>
+    Cloth Diaper<br></br>
+    Color: TopHat<br></br>
+    Price: $20.95<br></br>
+    <button>Purchase</button>
+    </div>
+    </div>
       </div>
     </div>
     );
