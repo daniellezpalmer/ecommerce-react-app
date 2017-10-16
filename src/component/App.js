6bf66565b068aac6ed891922242368e803e4ac93
Danielle Palmer <daniellezpalmer@gmail.com>
2017-10-16 09:21:15 -0500

    Styled app and some home component

--- src/component/App.js
+++ src/component/App.js
@@ -5,20 +5,30 @@ import image from '../images/dipes.png';
 class App extends Component {
 
   render() {
+    let app = {
+      textAlign:"center",
+      fontSize:40,
+      margin:"20px 0px 0px 0px"
+    }
 
     let img = {
       backgroundImage: `url(${image})`,
       height:500,
       backgroundRepeat:"no-repeat",
       display:"flex",
-      margin:"2% 30% 0% 30%"
+      margin:"2% 30% 0% 35%"
+    }
+
+    let link = {
+      textDecoration:"none",
+      fontSize:120,
+      textAlign:"center"
     }
 
     return (
-      <div className="App">
-      Welcome to
-      <div style={img}>Diapers R Us</div>
-      <Link exact to="/about">More about us!
+      <div style={app} className="App">
+      <div style={img}></div>
+      <Link style={link} exact to="/about">More about us!
       </Link>
       </div>
     );
