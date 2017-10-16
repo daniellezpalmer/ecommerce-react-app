37c5a90bd8b869b929e9d027dd919e404330ec6d
Danielle Palmer <daniellezpalmer@gmail.com>
2017-10-16 07:47:01 -0500

    created app component, imported link and linked it to (future) about component

--- component/App.js
+++ component/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      Welcome to
+      <div>Diapers R Us</div>
+      <Link exact to="/about">More about us!
+      </Link>
       </div>
     );
   }
