493dbd3330f7a9845303dbea668e19a7686bee6f
Danielle Palmer <daniellezpalmer@gmail.com>
2017-10-16 08:03:05 -0500

    imported all components to index.js, updated reactdom.render to include components wrapped in switch, baselayout and browserrouter

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ import './index.css';
 import App from './component/App.js';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import BaseLayout from './component/BaseLayout.js';
+import Home from './component/Home.js'
+import About from './component/About.js';
+import Contact from './component/Contact.js';
+import Cd from './component/ClothDiapers.js';
+import Pt from './component/PottyTraining.js';
+import Sd from './component/SwimDiapers.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <BaseLayout>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/home" component={Home} />
+        <Route path="/cloth" component={Cd} />
+        <Route path="/pottytraining" component={Pt} />
+        <Route path="/swim" component={Sd} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </BaseLayout>
+  </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
