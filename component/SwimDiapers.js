cc4b2cfad11158ae1a4589b89eae78347b03388b
Danielle Palmer <daniellezpalmer@gmail.com>
2017-10-16 08:01:24 -0500

    Created swimdiaper component, imported link, linked to home

---
+++ component/SwimDiapers.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Sd extends Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return (
+      <div className="Cd">
+        <div>
+            All Things Swim!<br></br>
+            <Link to="/home"><button>Home</button></Link>
+
+<div>
+  <div>
+  <div>
+  </div>
+  Swim Diaper<br></br>
+  Color: Sailor<br></br>
+  Price: $10.95<br></br>
+  <button>Purchase</button>
+</div>
+<div>
+  <div>
+  </div>
+  Swim Diaper<br></br>
+  Color: Pink<br></br>
+  Price: $10.95<br></br>
+  <button>Purchase</button>
+</div>
+<div>
+  <div>
+  </div>
+  Swim Diaper<br></br>
+  Color: Flowers<br></br>
+  Price: $10.95<br></br>
+  <button>Purchase</button>
+</div>
+</div>
+      </div>
+    </div>
+    );
+  }
+}
