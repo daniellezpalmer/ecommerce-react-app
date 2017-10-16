d5a115e6991e54f20d9ef01bd5318ccdbaccd4d9
Danielle Palmer <daniellezpalmer@gmail.com>
2017-10-16 09:38:03 -0500

    Styled contact component

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -3,8 +3,16 @@ import React, { Component } from 'react';
 export default class Contact extends Component {
 
   render() {
+
+    let style = {
+      fontFamily:"Amatic SC",
+      fontSize:70,
+      textAlign:"center",
+      margin:"10%"
+    }
+
     return (
-      <div className="Contact">
+      <div style={style} className="Contact">
         For questions or concerns please contact us at:<br></br>
         PHONE: (512) 555 5555<br></br>
         EMAIL: babiesrus@example.com<br></br>
