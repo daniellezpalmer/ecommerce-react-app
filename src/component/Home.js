a38381f81d3ad1ddf43b999108d08c057d393895
Danielle Palmer <daniellezpalmer@gmail.com>
2017-10-16 09:30:18 -0500

    Finished styling home

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import dipes from '../images/bebe.jpg'
 
 export default class Home extends Component {
 
   render() {
+
+    let img = {
+      backgroundImage:`url(${dipes})`,
+      backgroundRepeat:"no-repeat",
+      height:1600,
+      width:"100%",
+      textAlign:"center",
+      fontSize:45
+    }
+
     return (
-      <div className="Home">
+      <div style={img} className="Home">
         Your one stop shop for all your diapering needs!<br></br>
       </div>
     );
