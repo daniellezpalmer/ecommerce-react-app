7e3f85913903d3c374b4990a01d1303e1c90916f
Danielle Palmer <daniellezpalmer@gmail.com>
2017-10-16 07:44:09 -0500

    Added imports to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './component/App.js';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
